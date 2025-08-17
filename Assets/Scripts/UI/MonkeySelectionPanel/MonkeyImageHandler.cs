47604d4ba2dd7980f98eefca3855a2c739c69fc1
Tarun <tarunkumarsharma38@gmail.com>
2025-08-17 12:30:35 +0530

    added the  drag handler

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,13 +6,14 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour,IDragHandler
+    public class MonkeyImageHandler : MonoBehaviour,IDragHandler,IEndDragHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
         private RectTransform monkeyTransform;
-
+        private Vector3 origionalPosition;
+        private Vector3 anchoredPosition;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -25,11 +26,30 @@ namespace ServiceLocator.UI
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
             monkeyTransform = GetComponent<RectTransform>();
+            origionalPosition = monkeyTransform.localPosition;
+            anchoredPosition = monkeyTransform.anchoredPosition;
         }
 
         public void OnDrag(PointerEventData eventData)
         {
-            monkeyTransform.anchoredPosition+= eventData.delta;
+            monkeyTransform.anchoredPosition += eventData.delta;
+            owner.MonkeyDraggedAt(monkeyTransform.position);
         }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkey();
+            owner.MonkeyDroppedAt(eventData.position);
+        }
+
+        private void ResetMonkey()
+        {
+            monkeyTransform.position = origionalPosition;
+            monkeyTransform.anchoredPosition = anchoredPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+
+        
     }
 }
